05a7076f1addbb155f1930e9af409e47ffbe3e7e
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-07-16 08:09:14 +0500

    add users.js

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ app.get("/", (req, res) => {
   res.render("index", { text: "World" });
 });
 
+app.get("/users", (req, res) => {
+  res.send("User List");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("User New Form");
+});
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 app.listen(3000);
